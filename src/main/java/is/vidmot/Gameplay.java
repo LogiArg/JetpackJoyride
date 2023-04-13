029c20dfddcf3330372a91f1ab7c7bb9e0a75e65
LogiArg <logi.arg@gmail.com>
2023-04-13 13:27:11 +0000

    Initial commit

---
+++ src/main/java/is/vidmot/Gameplay.java
@@ -0,0 +1,14 @@
+package is.vidmot;/******************************************************************************
+ *  Nafn    : Logi Arnarsson
+ *  T-póstur: loa13@hi.is
+ *
+ *  Lýsing  : Lýsing á hvað forritið gerir, inntak, útreikningar, úttak 
+ *
+ *
+ *****************************************************************************/
+
+public class Gameplay {
+    public static void main(String[] args) {
+        
+    }
+}
